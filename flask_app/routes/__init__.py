6aaf6f42ae025ef5c813b7b8fa331c4464382f01
soroushh <soroush@tails.com>
2020-09-24 16:43:48 +0100

    We can add people and create list of them now.

--- flask_app/routes/__init__.py
+++ flask_app/routes/__init__.py
@@ -1,3 +1,4 @@
+from flask import render_template
 from flask_app import app, db
 from flask_app.models.person import Person
 
@@ -8,3 +9,8 @@ def people():
     db.session.add(person)
     db.session.commit()
     return 'Person created completely.'
+
+@app.route('/all_people')
+def all_people():
+    all_people = Person.query.all()
+    return render_template('all.html', people=all_people)
