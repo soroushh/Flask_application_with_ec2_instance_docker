15eae08b0f15a6c7b71ae332764861b027aeeddb
soroushh <soroush@tails.com>
2020-09-25 10:34:09 +0100

    The movement creation was created successfully.

--- flask_app/routes/__init__.py
+++ flask_app/routes/__init__.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from flask_app import app, db
-from flask_app.models.models import Person
+from flask_app.models.models import Person, Movement
 
 
 @app.route('/people')
@@ -23,3 +23,12 @@ def create_person(name, family, age):
 def all_people():
     all_people = Person.query.all()
     return render_template('all.html', people=all_people)
+
+@app.route('/create_movement/<name>/<int:repetition>/<int:person_id>')
+def create_movement(name, repetition,person_id):
+    """Creates a movement for a person."""
+    movement = Movement(name=name, person_id=person_id, repetition=repetition)
+    db.session.add(movement)
+    db.session.commit()
+    return 'model successfully created.'
+
