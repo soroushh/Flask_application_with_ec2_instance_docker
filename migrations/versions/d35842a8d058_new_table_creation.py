ab11c1e7e8889a1844355061d1bb63c1720ed0f6
Ubuntu <ubuntu@ip-172-31-43-230.eu-west-1.compute.internal>
2020-09-27 15:09:14 +0000

    New migration file is added.

---
+++ migrations/versions/d35842a8d058_new_table_creation.py
@@ -0,0 +1,24 @@
+"""new table creation
+
+Revision ID: d35842a8d058
+Revises: 
+Create Date: 2020-09-27 15:08:51.524107
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd35842a8d058'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
