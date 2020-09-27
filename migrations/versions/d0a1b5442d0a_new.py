425c6191e3df6b9565a35a7bbee347df535bd4c3
soroushh <soroush@tails.com>
2020-09-27 16:24:16 +0100

    The migration file is added.

---
+++ migrations/versions/d0a1b5442d0a_new.py
@@ -0,0 +1,24 @@
+"""new
+
+Revision ID: d0a1b5442d0a
+Revises: d35842a8d058
+Create Date: 2020-09-27 15:22:03.784426
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd0a1b5442d0a'
+down_revision = 'd35842a8d058'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
