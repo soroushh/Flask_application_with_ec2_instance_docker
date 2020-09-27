1434af3a7482512a668e37b3d0910aad3bf36891
Ubuntu <ubuntu@ip-172-31-43-230.eu-west-1.compute.internal>
2020-09-27 13:57:56 +0000

    The revision file is added.

---
+++ alembic/versions/848a0a9c1ca6_add_person_tabe.py
@@ -0,0 +1,24 @@
+"""Add person tabe
+
+Revision ID: 848a0a9c1ca6
+Revises: 
+Create Date: 2020-09-27 13:57:37.021421
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '848a0a9c1ca6'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
