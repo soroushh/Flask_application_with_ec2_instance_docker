ea0bf93e0df4de762584d02604b6425ddb5031dc
soroushh <soroush@tails.com>
2020-09-27 21:54:07 +0100

    The migration files are corrected now.

--- migrations/versions/d35842a8d058_new_table_creation.py
+++ migrations/versions/d35842a8d058_new_table_creation.py
@@ -17,13 +17,8 @@ depends_on = None
 
 
 def upgrade():
-    op.create_table(
-        'account',
-        sa.Column('id', sa.Integer, primary_key=True),
-        sa.Column('name', sa.String(50), nullable=False),
-        sa.Column('description', sa.Unicode(200)),
-    )
+    pass
 
 
 def downgrade():
-    op.drop_table('account')
+    pass
