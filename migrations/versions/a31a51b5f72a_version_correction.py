afef4a3627e0a69ab6c3a987829ef17013aa3fb6
soroushh <soroush@tails.com>
2020-10-04 17:46:25 +0100

    The correction migration file created.

---
+++ migrations/versions/a31a51b5f72a_version_correction.py
@@ -0,0 +1,24 @@
+"""version_correction
+
+Revision ID: a31a51b5f72a
+Revises: d0a1b5442d0a
+Create Date: 2020-10-04 16:45:23.487468
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1bab49b9420f'
+down_revision = 'd0a1b5442d0a'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
