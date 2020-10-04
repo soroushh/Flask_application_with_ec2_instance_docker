e6440311de58efd3b20f38da9db854f4933ac394
soroushh <soroush@tails.com>
2020-10-04 18:20:41 +0100

    The new migration file is created.

---
+++ migrations/versions/bd172bb970f0_new_file_created.py
@@ -0,0 +1,24 @@
+"""New file created
+
+Revision ID: bd172bb970f0
+Revises: a31a51b5f72a
+Create Date: 2020-10-04 17:20:24.266441
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'bd172bb970f0'
+down_revision = 'a31a51b5f72a'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
